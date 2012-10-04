1727abf95619619e7b07db64ca87aa868e12dadc
Cody Littlewood <littlewoodcody@gmail.com>
2012-10-04 12:19:54 -0300

    fixed scross.js

--- app/js/scroll.js
+++ app/js/scroll.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-  $(".scroll").click(function(event){
-    event.preventDefault();
-    var offset = $($(this).attr('href')).offset().top;
-    $('html, body').animate({scrollTop:offset}, 700);
-  });
+(function() {
+var scrollLink = $('a.scroll')
+scrollLink.on('click', function(event){
+event.preventDefault();
+$('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
 });
+})();
