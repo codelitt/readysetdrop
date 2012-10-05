e1854df790ac325650e8910c4b0262b732f43c2b
Cody Littlewood <littlewoodcody@gmail.com>
2012-10-05 14:21:24 -0300

    added tm

--- app/js/scroll.js
+++ app/js/scroll.js
@@ -2,6 +2,6 @@
 var scrollLink = $('a.scroll')
 scrollLink.on('click', function(event){
 event.preventDefault();
-$('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
+$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
 });
 })();
